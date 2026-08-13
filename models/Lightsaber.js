830d2105fdaf8ebcc0415a2e3b33bb10a5fa49f5
AminMtk <amin.mottaki@student.ehb.be>
2026-08-14 00:12:11 +0200

    Edit functie done

--- models/Lightsaber.js
+++ models/Lightsaber.js
@@ -4,25 +4,21 @@ const lightsaberSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    lowercase: true,
     trim: true
   },
   crystalColor: {
     type: String,   
     required: true,
-    lowercase: true,
     enum: ['blue', 'red', 'green', 'purple', 'yellow', 'white']
   },
   hiltType: {
     type: String,
     required: true,
-    lowercase: true,
     enum: ['single', 'double', 'crossguard']
   },
   ownerSide: {
     type: String,
     required: true,
-    lowercase: true,
     enum: ['Jedi', 'Sith']
   },
   createdAt: {
